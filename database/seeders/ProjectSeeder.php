4f430b41f44aee593feb79d5be802de0f3c5249d
matteomedori <matteo.medori.97@gmail.com>
2024-02-09 15:46:11 +0100

    feat: modifica seeder per aggiungere type_id

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -15,15 +16,19 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        for ($i = 0; $i < 20; $i++) {
-            $newproject = new Project();
 
+        Project::truncate();
+        for ($i = 0; $i < 20; $i++) {
+
+            $type = Type::inRandomOrder()->first();
+
+            $newproject = new Project();
             $newproject->title = $faker->sentence(3);
             $newproject->description = $faker->text(500);
             $newproject->slug = Str::of($newproject->title)->slug('-');
             $newproject->languages = $faker->randomElement(['HTML, CSS e JS', 'PHP']);
             $newproject->frameworks = $faker->randomElement(['Laravel', 'React', 'Vue', 'Angular', 'Bootstrap']);
-
+            $newproject->type_id = $type->id;
             $newproject->save();
         }
     }
