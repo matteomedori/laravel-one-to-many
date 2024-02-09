e715312f42939b9089ac1082d58c19c9f5bfb54a
matteomedori <matteo.medori.97@gmail.com>
2024-02-09 15:15:35 +0100

    feat: seeder per popolare types

---
+++ database/seeders/TypesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $types = ['Frontend', 'Backend', 'Full-stack'];
+
+        foreach ($types as $type) {
+            $new_type = new Type();
+
+            $new_type->name = $type;
+            $new_type->slug = Str::of($type)->slug('-');
+
+            $new_type->save();
+        }
+    }
+}
